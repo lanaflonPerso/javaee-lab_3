3716b7277e7f415498ac3a956df3fb0e01091d61
Vadym <vad.pechenoga@gmail.com>
2015-06-07 14:16:43 +0300

    Customer POJO

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -5,24 +5,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- * Product's category
- */
 @Entity
-public class Category {
+public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
-    private String description;
 
-    public Category() {
+    public Customer() {
     }
 
-    public Category(String name, String description) {
+    public Customer(String name) {
         this.name = name;
-        this.description = description;
     }
 
     public int getId() {
@@ -41,22 +36,14 @@ public class Category {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Category category = (Category) o;
+        Customer customer = (Customer) o;
 
-        if (name != null ? !name.equals(category.name) : category.name != null) return false;
+        if (name != null ? !name.equals(customer.name) : customer.name != null) return false;
 
         return true;
     }
@@ -68,6 +55,6 @@ public class Category {
 
     @Override
     public String toString() {
-        return "Category#" + id;
+        return "Customer#" + id;
     }
 }
